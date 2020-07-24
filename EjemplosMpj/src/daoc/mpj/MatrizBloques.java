967f4d2943b7d7a1477a30b0dffa6619b5a7b0ac
Diego Ordóñez <diego.ordonez@gmail.com>
2020-07-23 19:03:22 -0500

    Update

--- EjemplosMpj/src/daoc/mpj/MatrizBloques.java
+++ EjemplosMpj/src/daoc/mpj/MatrizBloques.java
@@ -4,6 +4,17 @@ import mpi.MPI;
 
 public class MatrizBloques {
 
+	/**
+	 * Este ejemplo está armado para una matriz original de 9x9 que se divide
+	 * en 9 submatrices de 3x3
+	 * Requiere que haya 9 procesos (MPI.COMM_WORLD.Size)
+	 * 
+	 * Podría funcionar para matrices cuadradas que puedan subdividirse en un 
+	 * número exacto de submatrices cuadradas. Luego debe haber un número de
+	 * procesos igual al número de submatrices
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		MPI.Init(args);
 		int rank = MPI.COMM_WORLD.Rank();
@@ -16,13 +27,17 @@ public class MatrizBloques {
 		MiMatriz[] mLocal = new MiMatriz[1];
 		
 		if(rank == master ) {
-			MiMatriz grande = new MiMatriz(matriz());
+			MiMatriz grande = MiMatriz.ejemplo01();
+			System.out.println(grande);
+			System.out.println("==========");
 			chicas = grande.divideEnBloques(numMat, bloque);
 			for(MiMatriz m : chicas) System.out.println(m);
+			System.out.println("==========");
 		}
 		
 		MPI.COMM_WORLD.Scatter(chicas, 0, 1, MPI.OBJECT, mLocal, 0, 1, MPI.OBJECT, master);
 		
+		//sacamos el promedio de todos los valores en la sumatriz recibida
 		double promedio = 0;
 		for(int f = 0; f < mLocal[0].getFilas(); f++) {
 			for(int c = 0; c < mLocal[0].getCols(); c++) {
@@ -30,6 +45,8 @@ public class MatrizBloques {
 			}
 		}
 		promedio /= (mLocal[0].getFilas() * mLocal[0].getCols());
+		
+		//reemplazamos todos los valores de la sumatriz, con el promedio calculado
 		for(int f = 0; f < mLocal[0].getFilas(); f++) {
 			for(int c = 0; c < mLocal[0].getCols(); c++) {
 				mLocal[0].setValor(promedio, f, c);
@@ -41,25 +58,12 @@ public class MatrizBloques {
 		
 		if(rank == master ) {
 			for(MiMatriz m : lasmatrices) System.out.println(m);
+			System.out.println("==========");
+			MiMatriz nueva = MiMatriz.reuneBloques(lasmatrices);
+			System.out.println(nueva);
 		}
-		
-		//System.out.println("Hola # " + rank + " de " + size);
+
 		MPI.Finalize();
 	}
 
-	private static double[][] matriz() {
-		double[][] matriz = {
-			{1, 2, 3, 10, 20, 30, 100, 200, 300},
-			{1, 2, 3, 10, 20, 30, 100, 200, 300},
-			{1, 2, 3, 10, 20, 30, 100, 200, 300},
-			{4, 5, 6, 40, 50, 60, 400, 500, 600},
-			{4, 5, 6, 40, 50, 60, 400, 500, 600},
-			{4, 5, 6, 40, 50, 60, 400, 500, 600},
-			{7, 8, 9, 70, 80, 90, 700, 800, 900},
-			{7, 8, 9, 70, 80, 90, 700, 800, 900},
-			{7, 8, 9, 70, 80, 90, 700, 800, 900}
-		};
-		return matriz;
-	}
-	
 }
--- EjemplosMpj/src/daoc/mpj/MiMatriz.java
+++ EjemplosMpj/src/daoc/mpj/MiMatriz.java
@@ -36,7 +36,7 @@ public class MiMatriz implements Serializable {
 		matriz[f][c] = valor;
 	}		
 	
-	MiMatriz subMatriz(int initF, int finF, int initC, int finC) {
+	MiMatriz getSubMatriz(int initF, int finF, int initC, int finC) {
 		MiMatriz subM = new MiMatriz(finF-initF+1, finC-initC+1);
 		for(int f = initF; f <= finF; f++) {
 			for(int c = initC; c <= finC; c++) {
@@ -46,15 +46,55 @@ public class MiMatriz implements Serializable {
 		return subM;
 	}
 	
+	void setSubMatriz(MiMatriz data, int initF, int finF, int initC, int finC) {
+		for(int f = initF; f <= finF; f++) {
+			for(int c = initC; c <= finC; c++) {
+				this.setValor(data.getValor(f-initF, c-initC), f, c);
+			}
+		}
+	}	
+	
 	MiMatriz[] divideEnBloques(int numMat, int bloque) {
 		MiMatriz[] chicas = new MiMatriz[numMat];
 		int counter = 0;
 		for(int f = 0; f < numMat; f+=bloque) {
 			for(int c = 0; c < numMat; c+=bloque) {
-				chicas[counter++] = this.subMatriz(f, f+bloque-1, c, c+bloque-1);
+				chicas[counter++] = this.getSubMatriz(f, f+bloque-1, c, c+bloque-1);
 			}
 		}
 		return chicas;
+	}
+	
+	static MiMatriz reuneBloques(MiMatriz[] chicas) {
+		int numMat = chicas.length;
+		int bloque = chicas[0].getCols();
+		int matLado = (int) Math.sqrt(numMat);
+		int cellLado = matLado * bloque;
+		MiMatriz nueva = new MiMatriz(cellLado, cellLado);
+		
+		int counter = 0;
+		for(int f = 0; f < numMat; f+=bloque) {
+			for(int c = 0; c < numMat; c+=bloque) {
+				nueva.setSubMatriz(chicas[counter++], f, f+bloque-1, c, c+bloque-1);
+			}
+		}
+		
+		return nueva;
+	}
+	
+	static MiMatriz ejemplo01 () {
+		double[][] matriz = {
+			{1, 2, 3, 10, 20, 30, 100, 200, 300},
+			{1, 2, 3, 10, 20, 30, 100, 200, 300},
+			{1, 2, 3, 10, 20, 30, 100, 200, 300},
+			{4, 5, 6, 40, 50, 60, 400, 500, 600},
+			{4, 5, 6, 40, 50, 60, 400, 500, 600},
+			{4, 5, 6, 40, 50, 60, 400, 500, 600},
+			{7, 8, 9, 70, 80, 90, 700, 800, 900},
+			{7, 8, 9, 70, 80, 90, 700, 800, 900},
+			{7, 8, 9, 70, 80, 90, 700, 800, 900}
+		};
+		return new MiMatriz(matriz);		
 	}
 	
 	@Override
